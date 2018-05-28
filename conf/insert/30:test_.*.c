87ba198373f6c0aca9daecf6a04b5c3b0a1d5f33
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2018-05-29 01:26:03 +0900

    dotfilesレポジトリから分離

---
+++ conf/insert/30:test_.*.c
@@ -0,0 +1,13 @@
+/**
+ * File:  `(file-name-nondirectory (buffer-file-name))`
+ * Author: ymiyamoto
+ *
+ * Created on `(current-time-string)`
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <criterion/criterion.h>
+
+$0
