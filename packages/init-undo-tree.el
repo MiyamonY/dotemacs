c5b4bc9b2e9b9bb87dd5c589e4d79647c20252a4
MiyamonY <y.miyamoto324@gmail.com>
2018-05-29 09:31:56 +0900

    undo-treeを追加

---
+++ packages/init-undo-tree.el
@@ -0,0 +1,13 @@
+;;; init-undo-tree.el --- initalize undo-tree        -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018
+
+;; Author:  <ymiyamoto@ymiyamoto-desktop>
+;; Keywords: lisp,
+(use-package undo-tree
+  :bind
+  (("C-." . undo-tree-redo)
+   :map undo-tree-visualizer-mode-map
+   ("C-m" . undo-tree-visualizer-quit)))
+
+(global-undo-tree-mode)
