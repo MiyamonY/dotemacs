0bafed444e2216420591ecc1afd70bf5bdb47670
MiyamonY <y.miyamoto324@gmail.com>
2018-06-02 19:01:46 +0900

    flycheck-pos-tipを追加

---
+++ packages/init-flycheck-pos-tip.el
@@ -0,0 +1,32 @@
+;;; init-flycheck-pos-tip.el --- initalize flycheck-pos-tip  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018
+
+;; Author:  <ymiyamoto@ymiyamoto-desktop>
+;; Keywords: abbrev
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+(use-package flycheck-pos-tip
+  :hook
+  ((flycheck-mode . flycheck-pos-tip-mode)))
+
+
+(provide 'init-flycheck-pos-tip)
+;;; init-flycheck-pos-tip.el ends here
