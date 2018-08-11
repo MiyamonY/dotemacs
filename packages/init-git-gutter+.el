c2bcea9a2b4673be78e0fbe1cc808e85768ceee5
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2018-08-12 01:38:18 +0900

    tramp上でうまくいかないためgit-gutter+の自動更新停止

--- packages/init-git-gutter+.el
+++ packages/init-git-gutter+.el
@@ -1,9 +1,4 @@
 (use-package git-gutter+
   :config
   (progn
-    (global-git-gutter+-mode t)
-    (defun my-git-gutter+-refresh ()
-      (if (git-gutter+-in-git-repository-p (buffer-file-name))
-          (git-gutter+-refresh)))
-    (run-with-idle-timer 1 t 'my-git-gutter+-refresh)
-    ))
+    (global-git-gutter+-mode t)))
