84a2fedbf11bf974265d4f9128e41a63a14921e5
MiyamonY <y.miyamoto324@gmail.com>
2019-06-07 21:43:15 +0900

    Add stting for show paren

--- lisp/util.el
+++ lisp/util.el
@@ -4,4 +4,11 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
+(show-paren-mode 1)
+(setq show-paren-dilay 0.2)
+(setq show-paren-style 'expression)
+(set-face-attribute 'show-paren-match nil
+		    :background 'unspecified :foreground 'unspecified
+		    :underline "#bd93f9")
+
 (provide 'util)
