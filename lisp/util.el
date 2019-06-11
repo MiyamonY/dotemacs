2d1f5fca2e8c7415be02a318d2bf4d854293c008
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2019-06-11 16:32:34 +0900

    Make sure to tailing-whitepace face and show-paren-match face applied

--- lisp/util.el
+++ lisp/util.el
@@ -1,18 +1,6 @@
 (setq jit-lock-defer-time 0.05)
 (setq-default bidi-display-reordering nil)
 
-(setq auto-save-list-file-prefix nil)
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-(tool-bar-mode -1)
-
-(show-paren-mode 1)
-(setq show-paren-dilay 0.2)
-(setq show-paren-style 'expression)
-(set-face-attribute 'show-paren-match nil
-		    :background 'unspecified :foreground 'unspecified
-		    :underline "#bd93f9")
-
 (setq default-frame-alist
       '((width . 120)
 	(height . 40)
@@ -21,11 +9,16 @@
 	(left . 0)
 	(font . "-PfEd-Ricty Diminished Discord-normal-normal-normal-*-*-*-*-*-*-0-iso10646-1")))
 
+(setq auto-save-list-file-prefix nil)
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(tool-bar-mode -1)
+
+(show-paren-mode 1)
+(setq show-paren-dilay 0.2)
+(setq show-paren-style 'expression)
+
 (setq-default show-trailing-whitespace t)
-(set-face-attribute 'trailing-whitespace nil
-		    :foreground 'unspecified
-		    :background 'unspecified
-		    :underline "red")
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 (delete-selection-mode t)
