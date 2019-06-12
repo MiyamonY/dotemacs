da3a85010988d3697b7492c1da35f07ca610cf06
MiyamonY <y.miyamoto324@gmail.com>
2019-06-12 14:03:01 +0900

    Improve font settings

--- lisp/util.el
+++ lisp/util.el
@@ -1,14 +1,6 @@
 (setq jit-lock-defer-time 0.05)
 (setq-default bidi-display-reordering nil)
 
-(setq default-frame-alist
-      '((width . 120)
-	(height . 40)
-	(vertical-scroll-bars . nil)
-	(top . 0)
-	(left . 0)
-	(font . "-PfEd-Ricty Diminished Discord-normal-normal-normal-*-*-*-*-*-*-0-iso10646-1")))
-
 (setq auto-save-list-file-prefix nil)
 (defalias 'yes-or-no-p 'y-or-n-p)
 
