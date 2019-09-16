bf25f61de6ac87cbd29a98671049354920729049
MiyamonY <y.miyamoto324@gmail.com>
2019-09-16 22:03:18 +0900

    Change face for adoc mode

--- init.el
+++ init.el
@@ -481,6 +481,11 @@
   (set-face-attribute 'markup-command-face nil :foreground "gray60" :weight 'bold)
   (set-face-attribute 'markup-value-face nil :foreground "gray60" :weight 'bold)
   (set-face-attribute 'markup-meta-face nil :foreground "gray60" :weight 'bold)
+  (set-face-attribute 'markup-title-0-face nil :height 2.0)
+  (set-face-attribute 'markup-title-1-face nil :height 1.5)
+  (set-face-attribute 'markup-title-2-face nil :height 1.2)
+  (set-face-attribute 'markup-title-3-face nil :height 1.0)
+  (set-face-attribute 'markup-title-4-face nil :height 1.0)
 
   (set-face-attribute 'markup-meta-hide-face nil :foreground "gray90" :weight 'bold)
   (set-face-attribute 'markup-list-face nil :background 'unspecified :weight 'bold))
