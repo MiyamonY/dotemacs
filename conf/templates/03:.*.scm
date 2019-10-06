bc5c9935711da21e44796433a729a4f2bb3d1a6f
MiyamonY <y.miyamoto324@gmail.com>
2019-10-07 01:54:11 +0900

    Update scheme template

--- conf/templates/03:.*.scm
+++ conf/templates/03:.*.scm
@@ -3,8 +3,31 @@
 ;;
 ;; Created on `(current-time-string)`
 ;;
-(define (read-number)
-  (string->number (read-line)))
+(define-syntax read-number
+  (syntax-rules ()
+    ((_ nums)
+     (define-values nums
+       (apply values (map string->number (string-split (read-line) #\space)))))))
 
-(define (read-numbers)
-  (map string->number (string-split (read-line) #\space)))
+(define-syntax read-numbers
+  (syntax-rules ()
+    ((_ as)
+     (define as (map string->number (string-split (read-line) #\space))))
+    ((_ as n)
+     (define as (map (lambda (_) (map string->number (string-split (read-line) #\space))) (iota n))))))
+
+(define-syntax prlist
+  (syntax-rules ()
+    ((_ lis)
+      (print (string-join (map number->string lis) " ")))))
+ 
+(define-syntax 1+ (syntax-rules () ((_ x) (+ x 1))))
+
+(define-syntax 1- (syntax-rules () ((_ x) (- x 1))))
+
+(define MOD 1000000007)
+
+(define (solve)
+	$0)
+
+(solve)
