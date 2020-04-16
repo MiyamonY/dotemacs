9bd135672ad5843528565ea7f934724b9343e107
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2020-04-17 01:14:04 +0900

    Update ocaml template

--- conf/templates/05:.*.ml
+++ conf/templates/05:.*.ml
@@ -1,20 +1,69 @@
 open Batteries
-open BatPrintf
+module EnumL = Enum.Labels
+module ListL = List.Labels
+module ArrayL = Array.Labels
 
-let scan fmt f =
-  BatScanf.sscanf (read_line ()) fmt f
+let dbg0 x = Printf.eprintf "[debug]%s\n" @@ dump x
+let dbg1 x = dbg0 x; x
+
+let id = identity
+
+let (++) n m = List.range n \`To m
+let (++^) n m = if n < m then List.range n \`To (pred m) else []
+
+let scan fmt = Scanf.sscanf (read_line ()) fmt
+
+let scan_lines n fmt f =
+  if n = 0 then Enum.empty ()
+  else
+    List.map (fun _ -> scan fmt f) (1 ++ n)
+    |> List.enum
 
 let scan_list cnv =
-  read_line ()
-  |> BatString.split_on_char ' '
+  String.split_on_char ' ' @@ read_line ()
   |> List.map cnv
 
-let rec zip xs ys =
-  match xs, ys with
-  | [], _ -> []
-  | _, [] -> []
-  | x::xs, y::ys ->
-    (x, y) :: zip xs ys
+let scan_array cnv = Array.of_list @@ scan_list cnv
+
+let scan_matrix n m e conv =
+  let arr = Array.make_matrix n m e in
+  ListL.iter (0 ++^ n)
+    ~f:(fun i -> arr.(i) <- Array.of_list @@ scan_list conv);
+  arr
+
+let rec powerset e =
+  match Enum.get e with
+  | None -> Enum.singleton @@ Enum.empty ()
+  | Some v ->
+    let f = powerset e in
+    let g = Enum.clone f in
+    EnumL.map f ~f:(fun x -> let y = Enum.clone x in push y v; y)
+    |>  Enum.append g
+
+let permutations l =
+  let rec aux l =
+    let rec interleave x = function
+      | [] -> [[x]]
+      | (hd::tl) as lst ->
+        (x::lst) ::
+        (ListL.map ~f:(List.cons hd)  @@ interleave x tl)
+    in
+    match l with
+    | [] -> [[]]
+    | hd::tl -> List.concat @@ List.map (interleave hd) @@ aux tl in
+  let l = List.sort (List.compare Int.compare) @@ aux @@ List.of_enum l in
+  List.enum % ListL.map ~f:List.enum @@ l
+
+let intersection l =
+  EnumL.filter ~f:(fun x -> EnumL.exists ~f:((=) x) @@ Enum.clone l)
+
+let lower_bound n f =
+  let rec aux l u =
+    if u - l > 1 then
+      let m = (l + u) / 2 in
+      if f m then aux l m
+      else aux m u
+    else u in aux (-1) n
 
 let () =
   $0
