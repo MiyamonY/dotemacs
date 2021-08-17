10dd6e9b6cbda7755533ed82a27cb1a9c6d8ebee
MIYAMOTO Yohei <miyamoto@heartorg.co.jp>
2021-08-17 21:36:48 +0900

    [Add][emacs] change org-mode bullets

--- init.el
+++ init.el
@@ -421,7 +421,7 @@
   (setq org-confirm-babel-evaluate nil))
 
 (use-package org-bullets
-  :custom (org-bullets-bullet-list '("" "" "" "" ""))
+  :custom (org-bullets-bullet-list '("" "" "" "" ""))
   :hook (org-mode . org-bullets-mode))
 
 (use-package lsp-mode
