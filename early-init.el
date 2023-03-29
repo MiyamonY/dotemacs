388fc2daf227e9555b4215139733dceabbf2e160
MiyamonY <y.miyamoto324@gmail.com>
2023-03-29 14:10:06 +0900

    [emacs] add: emacs29

---
+++ early-init.el
@@ -0,0 +1,7 @@
+;;; early-init.el ---                                -*- lexical-binding: t; -*-
+;; Copyright (C) 2023
+
+;; Author:  <ymiyamoto@ymiyamoto>
+;; Keywords: c, c, c, lisp,
+
+(setq native-comp-async-report-warnings-errors nil)
